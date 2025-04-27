9d1a844e713439fdb073dbb39a6fd3ad4077fe22
Tawan Silva <tawan.sf.99@gmail.com>
2025-04-26 21:57:45 -0300

    c learning

---
+++ c/Variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int age;
+	int age, year;
+
+	age = 26;
+	year = 2025;
+
+	int intVar = 1;
+	double doubleVar = 2.3444;
+	char charVar = 'T';
+
+	printf("%d", intVar);
+
+	return 0;
+}
