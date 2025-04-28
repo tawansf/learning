7b4226b71b1f152bd9c99582343bf90bde453514
Tawan Silva <tawan.sf.99@gmail.com>
2025-04-28 00:48:25 -0300

    tests

--- fsharp/FSharpTest/Program.fs
+++ fsharp/FSharpTest/Program.fs
@@ -1,1 +1,11 @@
-﻿printfn "Hello from F#"
+﻿let toPigLatin (word: string) =
+    let isVowel (c: char) =
+        match c with
+        | 'a' | 'e' | 'i' | 'o' | 'u'
+        | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+        |_ -> false
+    
+    if isVowel word[0] then
+        word + "yay"
+    else
+        word[1..] + string(word[0]) + "ay"
