f3b8bbdacaad0da7586118e38c5a74c01dc79c0f
Tawan Silva <tawan.sf.99@gmail.com>
2025-05-11 05:06:19 -0300

    math

---
+++ cplus/Math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Max: " << max(5, 10) << "\n";
+    cout << "Min: " << min(5, 10) << "\n";
+    cout << "Square Root: " << sqrt(25) << "\n";
+    cout << "Round a number: " << round(1.85) << "\n";
+    cout << "Log: " << log(10) << "\n";
+
+    return 0;
+}
